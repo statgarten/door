5a98f8d7f7753ace1bc82f5a712debff24df58a8
changwoolim <limcw@zarathu.com>
2022-11-16 15:45:59 +0900

    Add init()

---
+++ R/init.R
@@ -0,0 +1,19 @@
+#' Install statgarten dependencies.
+#'
+#' @description Install statgarten dependencies, datatoys, board, scissor, colorpen, soroban, stove
+#' @return an HTML list
+#'
+#' @examples
+#' init()
+#' @importFrom remotes install_github
+#' @export
+
+init <- function(){
+  # Install datatoys, board, scissor, colorpen, soroban, stove
+  remotes::install_github("statgarten/datatoys")
+  remotes::install_github("statgarten/board")
+  remotes::install_github("statgarten/scissor")
+  remotes::install_github("statgarten/colorpen")
+  remotes::install_github("statgarten/soroban")
+  remotes::install_github("statgarten/stove")
+}
